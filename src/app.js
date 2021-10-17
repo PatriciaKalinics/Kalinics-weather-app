254ea44861a2f5b04717f6e240cc59cf83eb971b
PatriciaKalinics <patriciakalinics@gmail.com>
2021-10-17 18:24:02 +0100

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -65,10 +65,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "10d1d5b699dafcc3bae98dd9eeecb91a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
